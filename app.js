d20a5bf1f64a21fa5ecf6457b635ea2a5c95d0bc
TrevorLovesMommy <melissa1lin@gmail.com>
2020-03-03 14:46:59 -0800

    updating homework

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-//populate table with data from data.js
+//Assign the data from data.js to a variable
+var data = data;
+
+//get reference to the button with the id "filter-btn"
+ var button = d3.select("#filter-btn");
 
 // Get a reference to the table body
 var tbody = d3.select("tbody");
@@ -6,9 +10,8 @@ var tbody = d3.select("tbody");
 //console log the data
 console.log(data);
 
-//Loop through data variable from data.js 
-
-
+//-----------populate html table with data from data.js----------------------------
+//the variable "data" was defined in data.js
 //create new td for every value in object
 data.forEach(function(alienReport){
     //use d3 to append one row per alien report
@@ -18,9 +21,33 @@ data.forEach(function(alienReport){
         var cell=row.append("td");
         //for every dell, pupluate the value
         cell.text(value);
-        
     });
 });
+
+//--------use the "on" function in d3 and inline function to record an event--------
+button.on("click", function() {
+    console.log("a button was clicked");
+    console.log(d3.event.target);
+
+    //get a reference to the input element on the page with the class "form-control"
+    var inputElement = d3.select(".form-control");
+    
+    //get the value property of the input element
+    var inputValue = inputElement.property("value");
+    console.log(`here's the input value ${inputValue}`);
+
+    //filter data based on inputValue
+    var filterdData = data.filter(report => report.datetime === inputValue);
+    console.log("here's the data");
+    console.log(filterdData);
+});
+
+// ----------- create a custom filtering fucntion based on input date -------------
+
+
+
+
+
 
 
 
