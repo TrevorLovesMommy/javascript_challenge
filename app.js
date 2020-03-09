b1b4f9a0f03f043d5ef18da3add84d8dcb20ce5a
TrevorLovesMommy <melissa1lin@gmail.com>
2020-03-09 10:21:03 -0700

    updating homework

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Assign the data from data.js to a variable
-var data = data;
+var tableData = data;
 
 //get reference to the button with the id "filter-btn"
  var button = d3.select("#filter-btn");
@@ -7,27 +7,33 @@ var data = data;
 // Get a reference to the table body
 var tbody = d3.select("tbody");
 
-//console log the data
-console.log(data);
+//console log tableData
+console.log(tableData);
 
-//-----------populate html table with data from data.js----------------------------
-//the variable "data" was defined in data.js
-//create new td for every value in object
-data.forEach(function(alienReport){
-    //use d3 to append one row per alien report
-    var row = tbody.append("tr");
-    //use d3 to append a cell for each value in the alien report object
-    Object.entries(alienReport).forEach(function([key,value]){
-        var cell=row.append("td");
-        //for every dell, pupluate the value
-        cell.text(value);
+//populate talble with UFO sightings
+function populateTable(info) {
+    tbody.html("");
+    console.log("in the populate table function");
+    info.forEach(function(alienReport){
+        //use d3 to append one row per alien report
+        console.log("appending row");
+        var row = tbody.append("tr");
+        //use d3 to append a cell for each value in the alien report object
+        Object.entries(alienReport).forEach(function([key,value]){
+            var cell=row.append("td");
+            //for every cell, pupluate the value
+            cell.text(value);
+        });
     });
-});
+}
+
+//Initial population of data
+populateTable(tableData);
 
 //--------use the "on" function in d3 and inline function to record an event--------
 button.on("click", function() {
     console.log("a button was clicked");
-    console.log(d3.event.target);
+ 
 
     //get a reference to the input element on the page with the class "form-control"
     var inputElement = d3.select(".form-control");
@@ -37,34 +43,25 @@ button.on("click", function() {
     console.log(`here's the input value ${inputValue}`);
 
     //filter data based on inputValue
-    var filterdData = data.filter(report => report.datetime === inputValue);
+    var filteredData = tableData.filter(report => report.datetime === inputValue);
     console.log("here's the data");
-    console.log(filterdData);
+    console.log(filteredData);
 
-    ////remove all rows https://stackoverflow.com/questions/7271490/delete-all-rows-in-an-html-table
-    d3.select("tbody").remove();
+    ////This did not work to clear and reload filtered data
+    // d3.select("tbody").remove();
+    // tbody.html("");
 
+    populateTable(filteredData);
 
-     
 });
 
-inputValue.forEach(function(alienReport){
-    //use d3 to append one row per alien report
-    var row = tbody.append("tr");
-    //use d3 to append a cell for each value in the alien report object
-    Object.entries(alienReport).forEach(function([key,value]){
-        var cell=row.append("td");
-        //for every dell, pupluate the value
-        cell.text(value);
-    });
-});
+// d3.select("#filter-btn").on("click", buttonClick);
+// // Build table with data.js
+// buildTable(tableData);
 
 
-
-
-
-
-
-
-
+// var clearEntries = d3.select("#reset-btn");
+// clearEntries.on("click", function() {
+//   location.reload();
+// });
 
